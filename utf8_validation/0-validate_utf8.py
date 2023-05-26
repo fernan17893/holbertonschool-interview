ce54eb495e2f24b9625bf8703ca1418fb63bb0f4
fernan17893 <fernan17893@gmail.com>
2023-05-25 22:34:59 -0400

    full push 1st stry

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""UTF-8 Validation"""
+
+
+def validUTF8(data):
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+    Args:
+        data: A list of integers, where each integer represents 1 byte of data.
+    Returns:
+        True if data is a valid UTF-8 encoding, else return False.
+    """
+
+    # Check if the data set is empty.
+    if not data:
+        return False
+
+    # Check if the first byte of the data set is a valid start byte.
+    start_byte = data[0]
+    if not (start_byte & 0x80 == 0):
+        return False
+
+    # Check the remaining bytes of the data set.
+    for i in range(1, len(data)):
+        byte = data[i]
+        if not (byte & 0xC0 == 0x80):
+            return False
+
+    return True
