8c2ab489a2377f3fdf24ca30a6c13cec385cf7d7
fernan17893 <fernan17893@gmail.com>
2023-05-25 22:39:49 -0400

    fix fix fix

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -10,20 +10,31 @@ def validUTF8(data):
     Returns:
         True if data is a valid UTF-8 encoding, else return False.
     """
+    bit1 = 1 << 7
+    bit2 = 1 << 6
+    nbytes = 0
 
-    # Check if the data set is empty.
-    if not data:
-        return False
+    if not data or len(data) == 0:
+            return True
 
-    # Check if the first byte of the data set is a valid start byte.
-    start_byte = data[0]
-    if not (start_byte & 0x80 == 0):
-        return False
+    for num in data:
+            bit = 1 << 7
+            if nbytes == 0:
+                while (bit & num):
+                    nbytes += 1
+                    bit = bit >> 1
 
-    # Check the remaining bytes of the data set.
-    for i in range(1, len(data)):
-        byte = data[i]
-        if not (byte & 0xC0 == 0x80):
+                if nbytes == 0:
+                    continue
+                if nbytes == 1 or nbytes > 4:
+                    return False
+            else:
+
+                if not (num & bit1 and not (num & bit2)):
+                    return False
+            nbytes -= 1
+
+    if nbytes:
             return False
-
-    return True
+    else:
+            return True
