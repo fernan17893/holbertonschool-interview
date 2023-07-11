fd5160dee4629698cf8c14e245eed481cfce27d2
fernan17893 <fernan17893@gmail.com>
2023-07-11 14:37:56 -0400

    not done

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,10 +1,12 @@
-const request = require("request");
+#!/usr/bin/node
+
+const request = require('request');
 
 const movieId = process.argv[2];
 
 const url = `https://swapi.dev/api/films/${movieId}`;
 
-request(url, async (error, response, body) => {
+request(url, function (error, response, body) {
   if (error) {
     console.error(error);
     return;
