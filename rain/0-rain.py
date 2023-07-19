e5fc5882fdf663972cda2dbf323f914e2d1f2594
fernan17893 <fernan17893@gmail.com>
2023-07-19 14:07:21 -0400

    code fix 1

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3
 """ Rain """
 
-def rain(walls) -> int:
-    """ Rain """
-    if walls is None:
+
+def rain(walls):
+    """Rain task"""
+    if not walls or not isinstance(walls, list):
         return 0
-    if len(walls) == 0:
-        return 0
-    if len(walls) == 1:
-        return 0
-    rain = 0
-    for i in range(1, len(walls) -1):
-        left = walls[i]
-        for j in range(i):
-            left = max(left, walls[j])
-        right = walls[i]
-        for j in range(i + 1, len(walls)):
-            right = max(right, walls[j])
-        rain = rain + (min(left, right) - walls[i])
-        return rain
+
+    l, water = len(walls), 0
+    left, right = [0] * l, [0] * l
+
+    left[0], right[l - 1] = walls[0], walls[l - 1]
+
+    i, j = 1, l - 2
+
+    while True:
+        if i >= l or j <= -1:
+            break
+
+        left[i] = max(left[i - 1], walls[i])
+        right[j] = max(right[j + 1], walls[j])
+
+        i += 1
+        j -= 1
+
+    for i in range(l):
+        water += min(left[i], right[i]) - walls[i]
+
+    return water
