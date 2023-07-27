a0e5d59ff79238c53b15edeb3055a68a4eb90dd3
fernan17893 <fernan17893@gmail.com>
2023-07-27 15:11:47 -0400

    header

---
+++ heap_sort/sort.h
@@ -0,0 +1,12 @@
+#ifndef sort_h
+#define sort_h
+
+
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void heapify(int *array, int n, int i, int size);
+
+
+
+
+#endif /* sort_h */
