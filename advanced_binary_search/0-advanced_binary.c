46185512e5f3094a271921eaca9d3cd16fdc4900
fernan17893 <fernan17893@gmail.com>
2023-08-04 14:36:59 -0400

    binary advanced

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -1,30 +1,45 @@
+#include "search_algos.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "search_algos.h"
+#include <stddef.h>
+
+
 
 /**
- * main - Entry point
- *
- * Return: Always EXIT_SUCCESS
+ * advanced_binary - realize a advanced_binary search
+ * @array: list of ints numbers
+ * @size: number of elements
+ * @value: value for search
+ * Return: -1 or value index
  */
-
 int advanced_binary(int *array, size_t size, int value)
 {
-    int i = 0;
-    int last = size - 1;
-    int mid = (i + last) / 2;
+	size_t i, mid, idx = 0;
+	int aux;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while (i <= last) {
-        if (array[mid] < value)
-            i = mid + 1;
-        else if (array[mid] == value)
-            return (mid);
-        else
-            last = mid - 1;
-        mid = (i + last) / 2;
-    }
-    return (-1);
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+		i == size - 1 ? printf("%d\n", array[i]) : printf("%d, ", array[i]);
+
+	if (size == 1 && array[0] != value)
+		return (-1);
+	mid = (size - 1) / 2;
+	if (array[mid] == value)
+	{
+		if (array[mid - 1] < value)
+			return (mid);
+	}
+	if (array[mid] < value)
+	{
+		idx += mid + 1, array += mid + 1;
+		size % 2 != 0 ? mid-- : 0;
+	}
+	mid++;
+	aux = advanced_binary(array, mid, value);
+	if (aux != -1)
+		return (aux + idx);
+	return (-1);
 }
