7b7cc9c1a98226b4e80219a3d3f273859176af9f
fernan17893 <fernan17893@gmail.com>
2023-08-23 11:08:58 -0400

    real push

--- heap_extract/binary_trees.h
+++ heap_extract/binary_trees.h
@@ -1,8 +1,8 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
-
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -14,16 +14,17 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
 typedef struct binary_tree_s heap_t;
+
 void binary_tree_print(const binary_tree_t *);
 int heap_extract(heap_t **root);
 
-
-#endif /* _BINARY_TREES_H_ */
+#endif /* BINARY_TREES_H */
