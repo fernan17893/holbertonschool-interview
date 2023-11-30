da272961bd8313666a12d935f03fe0724e65fc89
fernan17893 <fernan17893@gmail.com>
2023-11-30 11:18:57 -0400

    prime game

---
+++ primegame/0-prime_game.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Prime Game """
+
+def isWinner(x, nums):
+    """ Prime Game"""
+    if not nums or x < 1:
+        return None
+    n = max(nums)
+    sieve = [True] * (n + 1)
+    sieve[0] = False
+    sieve[1] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if sieve[i] is True:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+    primes = []
+    for i in range(len(sieve)):
+        if sieve[i] is True:
+            primes.append(i)
+    player1 = 0
+    for i in nums:
+        if i in primes:
+            player1 += 1
+    if player1 % 2 == 0:
+        return "Maria"
+    return "Ben"
