be8af77b4a93204e5431a3d59117959244f2660e
fernan17893 <fernan17893@gmail.com>
2023-11-30 11:28:17 -0400

    newer code push

--- primegame/0-prime_game.py
+++ primegame/0-prime_game.py
@@ -3,24 +3,40 @@
 
 def isWinner(x, nums):
     """ Prime Game"""
-    if not nums or x < 1:
-        return None
-    n = max(nums)
-    sieve = [True] * (n + 1)
-    sieve[0] = False
-    sieve[1] = False
-    for i in range(2, int(n ** 0.5) + 1):
-        if sieve[i] is True:
-            for j in range(i * i, n + 1, i):
-                sieve[j] = False
-    primes = []
-    for i in range(len(sieve)):
-        if sieve[i] is True:
-            primes.append(i)
-    player1 = 0
-    for i in nums:
-        if i in primes:
-            player1 += 1
-    if player1 % 2 == 0:
+    maria_wins = 0
+    ben_wins = 0
+
+    for i in range(x):
+        n = nums.pop()
+        can_move = False
+
+    for num in nums:
+        if isPrime(num):
+            can_move = True
+            nums.remove(num)
+            for multiple in range(num * 2, n + 1, num):
+                    nums.remove(multiple)
+            break
+
+    if can_move:
+        maria_wins += 1
+    else:
+        ben_wins += 1
+
+    if maria_wins > ben_wins:
         return "Maria"
-    return "Ben"
+    if ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
+    
+def isPrime(n):
+    """ Prime Game """
+    if n <= 1:
+        return False
+    
+    for i in range(2, int(num ** 0.5) + 1):
+        if n % i == 0:
+            return False
+        
+    return True
